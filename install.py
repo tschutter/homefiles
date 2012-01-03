55d8a28d6ca7a5373b1a8515efaf0b8b7eb5499a
Tom Schutter <t.schutter@comcast.net>
2012-01-03 16:59:57 -0700

    added mailcap

--- install.py
+++ install.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python
 #
-# $Id: build.py 9335 2011-06-21 19:31:44Z tschutter $
-#
-# Copyright 2008-2010 CoreLogic.
+# Copyright (c) 2012-2012 Tom Schutter
 # All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#    - Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    - Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials provided
+#      with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 #
 
 """
-Installs dotfiles using symbolic links.
+Installs dotfiles in tschutter/homefiles using symbolic links.
 """
 
 from optparse import OptionParser
@@ -94,6 +116,7 @@ def main():
     options.dotfilesDir = os.path.dirname(os.path.abspath(__file__))
 
     make_link(options, "gitconfig")
+    make_link(options, "mailcap")
     make_link(options, "mg")
     make_link(options, "pylintrc")
     make_link(options, "tmux.conf")
