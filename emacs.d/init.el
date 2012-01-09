5485a34d2d1f7fb46536020782b1269c09f4c854
Tom Schutter <t.schutter@comcast.net>
2012-01-09 09:01:12 -0700

    respawn the *scratch* buffer if it is killed

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,6 +22,29 @@
 ;;; Turn off blinking cursor.
 (blink-cursor-mode 0)
 
+;;; Respawn the scratch buffer if it is killed (C-x k).
+(save-excursion
+  (set-buffer (get-buffer-create "*scratch*"))
+  (make-local-variable 'kill-buffer-query-functions)
+  (add-hook 'kill-buffer-query-functions 'kill-scratch-buffer))
+(defun kill-scratch-buffer ()
+  ;; The next line is just in case someone calls this manually
+  (set-buffer (get-buffer-create "*scratch*"))
+
+  ;; Kill the current (*scratch*) buffer
+  (remove-hook 'kill-buffer-query-functions 'kill-scratch-buffer)
+  (kill-buffer (current-buffer))
+
+  ;; Make a brand new *scratch* buffer
+  (set-buffer (get-buffer-create "*scratch*"))
+  (lisp-interaction-mode)
+  (make-local-variable 'kill-buffer-query-functions)
+  (add-hook 'kill-buffer-query-functions 'kill-scratch-buffer)
+
+  ;; Since we killed it, don't let caller do that.
+  nil)
+(setq initial-scratch-message nil)  ;we know what the scratch buffer is for
+
 ;;; Default to filename at point for C-x C-f.
 (require 'ffap)
 (ffap-bindings)
