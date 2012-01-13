432a8871582d698820c4ad365010e2256df7f8dc
Tom Schutter <t.schutter@comcast.net>
2012-01-13 09:18:17 -0700

    added pycheckrc

--- install.py
+++ install.py
@@ -154,6 +154,7 @@ def link_dotfiles(options):
     make_dot_link(options, file_in_path("mail"), "mailcap")
     make_dot_link(options, file_in_path("mg"), "mg")
     make_dot_link(options, True, "profile")
+    make_dot_link(options, file_in_path("pycheck"), "pycheckrc")
     make_dot_link(options, file_in_path("pylint"), "pylintrc")
     make_dot_link(options, file_in_path("screen"), "screenrc")
     make_dot_link(options, file_in_path("tmux"), "tmux.conf")
