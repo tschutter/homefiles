5cd285d72485358b3285e00c2d8803f2e6816ef2
Tom Schutter <t.schutter@comcast.net>
2012-01-19 07:46:39 -0700

    fixed if test for windows-nt in last rev

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -25,25 +25,26 @@
 
 ;;;; Web browsing
 (if (not (eq system-type 'windows-nt))
-    (require 'w3m-load)
-    (setq browse-url-browser-function 'w3m-browse-url)
-    (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
-    ;;; Save the w3m buffers between sessions with desktop.el.
-    (defun w3m-register-desktop-save ()
-      "Set `desktop-save-buffer' to a function returning the current URL."
-      (setq desktop-save-buffer (lambda (desktop-dirname) w3m-current-url)))
-    (add-hook 'w3m-mode-hook 'w3m-register-desktop-save)
-    (defun w3m-restore-desktop-buffer (d-b-file-name d-b-name d-b-misc)
-      "Restore a `w3m' buffer on `desktop' load."
-      (when (eq 'w3m-mode desktop-buffer-major-mode)
-        (let ((url d-b-misc))
-          (when url
-            (require 'w3m)
-            (if (string-match "^file" url)
-                (w3m-find-file (substring url 7))
-              (w3m-goto-url-new-session url))
-            (current-buffer)))))
-    (add-to-list 'desktop-buffer-mode-handlers '(w3m-mode . w3m-restore-desktop-buffer)))
+    (progn
+      (require 'w3m-load)
+      (setq browse-url-browser-function 'w3m-browse-url)
+      (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+      ;;; Save the w3m buffers between sessions with desktop.el.
+      (defun w3m-register-desktop-save ()
+        "Set `desktop-save-buffer' to a function returning the current URL."
+        (setq desktop-save-buffer (lambda (desktop-dirname) w3m-current-url)))
+      (add-hook 'w3m-mode-hook 'w3m-register-desktop-save)
+      (defun w3m-restore-desktop-buffer (d-b-file-name d-b-name d-b-misc)
+        "Restore a `w3m' buffer on `desktop' load."
+        (when (eq 'w3m-mode desktop-buffer-major-mode)
+          (let ((url d-b-misc))
+            (when url
+              (require 'w3m)
+              (if (string-match "^file" url)
+                  (w3m-find-file (substring url 7))
+                (w3m-goto-url-new-session url))
+              (current-buffer)))))
+      (add-to-list 'desktop-buffer-mode-handlers '(w3m-mode . w3m-restore-desktop-buffer))))
 
 
 ;;;; Editor behavior
