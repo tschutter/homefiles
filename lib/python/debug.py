1e8261f7d970d928734a2aa887fb9a26263ccc3b
Tom Schutter <t.schutter@comcast.net>
2012-01-19 20:30:26 -0700

    fixed logic error in debug.py

--- lib/python/debug.py
+++ lib/python/debug.py
@@ -10,19 +10,19 @@ debugging session if an exception is raised and is uncaught.
 import sys
 
 
-def info(extype, value, traceback):
+def info(extype, value, tback):
     """Custom sys.excepthook."""
     if hasattr(sys, 'ps1') or not sys.stderr.isatty():
         # We are in interactive mode or we don't have a tty-like
         # device, so we call the default hook
-        sys.__excepthook__(extype, value, traceback)
+        sys.__excepthook__(extype, value, tback)
     else:
         import pdb
         import traceback
-        # We are NOT in interactive mode, print the exception.
-        traceback.print_exception(extype, value, traceback)
+        # We are not in interactive mode, print the exception.
+        traceback.print_exception(extype, value, tback)
         print
         # Start the debugger in post-mortem mode.
-        pdb.post_mortem(traceback)
+        pdb.post_mortem(tback)
 
 sys.excepthook = info
