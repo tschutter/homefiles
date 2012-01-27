b85836a53e65e14c3eadc7877d93861397a86e8c
Tom Schutter <t.schutter@comcast.net>
2012-01-27 13:19:01 -0700

    added pdb history

---
+++ pdbrc.py
@@ -0,0 +1,15 @@
+# Loaded by ~/.pdbrc, because .pdbrc can only contain pdb commands.
+
+# Command line history.
+import atexit, os, readline
+histfile = os.path.expanduser("~/.homefiles/var/pdb-history")
+try:
+    readline.read_history_file(histfile)
+except IOError:
+    pass
+atexit.register(readline.write_history_file, histfile)
+del histfile
+readline.set_history_length(200)
+
+# Cleanup any variables that could otherwise clutter up the namespace.
+del atexit, os, readline
