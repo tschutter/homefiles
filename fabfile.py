172f144490e3de2b7d98ba93cd38a6f65c07dc2f
Tom Schutter <t.schutter@comcast.net>
2012-03-03 09:30:53 -0700

    env.hosts -> fabric.api.hosts(DEPLOY_HOSTS)

--- fabfile.py
+++ fabfile.py
@@ -4,19 +4,19 @@
 Fabric fabfile for .homefiles.
 """
 
-from fabric.api import cd, env, run
+import fabric.api
 import paramiko
 import socket
 
 # Run in a non-interactive mode, calling abort anytime it would
 # normally prompt the user for input.
-env.abort_on_prompts = True
+fabric.api.env.abort_on_prompts = True
 
 # We must run the shell as a login shell (-l) so that install.py can
 # find files in ~/bin/
-env.shell = "/bin/sh -l -c"
+fabric.api.env.shell = "/bin/sh -l -c"
 
-env.hosts = [
+DEPLOY_HOSTS = [
     "crookshanks",
     "deadeye",
     "missy",
@@ -37,17 +37,17 @@ def _is_host_up(host, port):
     try:
         paramiko.Transport((host, port))
         host_status = True
-    except:
+    except Exception:
         print "[%s] WARNING: Cannot connect to port %s." % (host, port)
     socket.setdefaulttimeout(original_timeout)
     return host_status
 
 
 #@parallel # waiting for fab-1.3
+@fabric.api.hosts(DEPLOY_HOSTS)
 def deploy():
     """Deploy .homefiles updates to known hosts."""
-    if _is_host_up(env.host, int(env.port)):
-        homefiles_dir = "~/.homefiles"
-        with cd(homefiles_dir):
-            run("git pull")
-            run("python install.py")
+    if _is_host_up(fabric.api.env.host, int(fabric.api.env.port)):
+        with fabric.api.cd("~/.homefiles"):
+            fabric.api.run("git pull")
+            fabric.api.run("python install.py")
