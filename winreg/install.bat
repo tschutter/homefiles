ddefa67c65f8a98e6a0c8cb9bf7dec409510765d
Tom Schutter <t.schutter@comcast.net>
2012-03-10 06:47:20 -0700

    initial checkin of winreg/install.bat

---
+++ winreg/install.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem winreg emacs.reg
+rem winreg no-hide-known-file-extensions.reg
+rem winreg no-language-bar.reg
+rem winreg no-recycle-bin.reg
+rem winreg no-shortcut-text.reg
+rem winreg no-shutdown-event-tracker.reg
+rem winreg verbose-boot.reg
