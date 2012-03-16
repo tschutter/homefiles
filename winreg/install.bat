dd163ffa685076dd1132dafe797aba4c609a6801
Tom Schutter <t.schutter@comcast.net>
2012-03-16 12:28:58 -0600

    improved and validated no-screen-saver.reg

--- winreg/install.bat
+++ winreg/install.bat
@@ -4,7 +4,7 @@ rem regedit /s emacs.reg
 rem regedit /s no-hide-known-file-extensions.reg
 rem regedit /s no-language-bar.reg
 rem regedit /s no-recycle-bin.reg
-rem regedit /s no-screen-saver.reg
+regedit /s no-screen-saver.reg
 rem regedit /s no-shortcut-text.reg
 rem regedit /s no-shutdown-event-tracker.reg
 rem regedit /s verbose-boot.reg
