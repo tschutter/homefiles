d649c97d03da352e2485e0968fa0336b4714a009
Tom Schutter <t.schutter@comcast.net>
2021-06-25 19:50:02 -0600

    uboat.py: fixed pycheck warnings

--- mutt/uboat.py
+++ mutt/uboat.py
@@ -1,31 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-# uboat.py --- generate u-boat-death messages, patterned after Iron Coffins
+"""
+Generate u-boat-death messages, patterned after Iron Coffins.
 
-# Author: Brian W. Fitzpatrick <fitz@red-bean.com>
-# Original elisp author: Bob Manson <manson@cygnus.com>
-# Original elisp maintainer: Noah Friedman <friedman@prep.ai.mit.edu>
-# Keywords: extensions
-# Created: 2010-04-21
-# Originally created: 1995-08-02
+Author: Brian W. Fitzpatrick <fitz@red-bean.com>
+Original elisp author: Bob Manson <manson@cygnus.com>
+Original elisp maintainer: Noah Friedman <friedman@prep.ai.mit.edu>
+Keywords: extensions
+Created: 2010-04-21
+Originally created: 1995-08-02
 
-# Bozoup(P) 1995 The Bozo(tic) Softwar(e) Founda(t)ion, Inc.
-# See the BOZO Antipasto for further information.
-# If this is useful to you, may you forever be blessed by the Holy Lord
-# Patty.  AT&T you will.
+Bozoup(P) 1995 The Bozo(tic) Softwar(e) Founda(t)ion, Inc.
+See the BOZO Antipasto for further information.
+If this is useful to you, may you forever be blessed by the Holy Lord
+Patty.  AT&T you will.
 
-# Commentary:
+Commentary:
 
-# Bob Manson originally wrote this program in ksh,
-# then later converted it to emacs lisp.
-# Mark Welch contributed additional items to the dictionary.
-# Noah Friedman rewrote parts of the engine.
-# Brian W. Fitzpatrick rewrote it in Python
+Bob Manson originally wrote this program in ksh,
+then later converted it to emacs lisp.
+Mark Welch contributed additional items to the dictionary.
+Noah Friedman rewrote parts of the engine.
+Brian W. Fitzpatrick rewrote it in Python.
+Tom Schutter cleaned the Python.
+"""
 
-from __future__ import print_function
 import random
 
+
 class Uboat():
+    """Uboat message generator."""
+
     def __init__(self):
         self._uboat_message = [
             ['*verb', 'BY', '*device.', '*sinkage.'],
@@ -135,32 +140,27 @@ class Uboat():
             '122W 41N',
         ]
 
-
     def _parse_template(self, template):
         message = []
         tail = ''
         for element in template:
-            if element[0] is '*': # Needs to be replaced
-                if element[-1] is '.': # maintain punctuation
+            if element[0] == '*':  # Needs to be replaced
+                if element[-1] == '.':  # maintain punctuation
                     ary = '_uboat_' + element[1:-1]
                     tail = element[-1]
                 else:
                     ary = '_uboat_' + element[1:]
 
                 chosen = random.choice(getattr(self, ary))
-                if type(chosen) is type([]):
-                    message.append(' '.join(self._parse_template(chosen)) + tail)
-                else:
-                    message.append(chosen + tail)
+                if isinstance(chosen, list):
+                    chosen = ' '.join(self._parse_template(chosen))
+                message.append(chosen + tail)
             else:
                 message.append(element)
         return message
 
-
     def death_message(self):
-        """Returns a u-boat-death message.
-        Returns a u-boat-death message patterned after messages in Iron
-        Coffins."""
+        """Return a u-boat-death message."""
         template = random.choice(self._uboat_message)
         message = self._parse_template(template)
         message.append('U-' + str(random.randint(100, 999)) + '.')
